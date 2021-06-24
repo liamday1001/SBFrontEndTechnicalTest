3b63ef9bfcc59d0fc09d931cc06078f2537366ef
Liam Day <job@liamday.net>
2021-06-24 20:24:44 +0100

    Grid, search box and button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { DataService } from './data.service';
+import * as wjcCore from '@grapecity/wijmo';
+import * as wjcGrid from '@grapecity/wijmo.grid';
+import * as wjcGridFilter from '@grapecity/wijmo.grid.filter';
+import * as wjcGridSearch from '@grapecity/wijmo.grid.search';
+import { zip } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  @ViewChild('flexgrid') flex?: wjcGrid.FlexGrid;
   title = 'SBFrontEndTechnicalTest';
+  filter: string = "";
+  collectionView: wjcCore.CollectionView = new wjcCore.CollectionView([]);
+  usersFormatted: any[] = [];
+
+  constructor(private data: DataService) { }
+
+  getUsers() {
+    this.data.getUsers().subscribe( res => {
+      this.usersFormatted = this.formatUsers(res);
+      this.collectionView = new wjcCore.CollectionView(this.usersFormatted);
+    });
+  }
+
+  ngOnInit() {
+    this.getUsers();
+
+  }
+
+  initGrid(grid: wjcGrid.FlexGrid) {
+    grid.collectionView.filter = (item: any) => {
+      if (item['name'].toLowerCase().indexOf(this.filter.toLowerCase()) >= 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    grid.collectionView.sortDescriptions.splice(0, 0, new wjcCore.SortDescription('name', true));
+  }
+
+  formatUsers(users: any[]): any[] {
+    var usersFormatted: any[] = [];
+
+    users.forEach(x => usersFormatted.push({"name": x.name, 
+                                             "age": x.age, 
+                                             "registered": new Date(x.registered.substr(0, 19)), 
+                                             "email": x.email, 
+                                             "balance": Number(x.balance.replace(',', '')) }))
+    return usersFormatted;
+
+  }
+
+  zeroBalance() {
+    this.usersFormatted.forEach(x => x.balance = 0);
+    this.flex?.invalidate(false);
+  }
+
+  inputKeyup() {
+    this.flex?.collectionView.refresh();
+    this.flex?.invalidate(false);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'; 
+import { WjGridModule } from '@grapecity/wijmo.angular2.grid';
+import { WjGridFilterModule } from '@grapecity/wijmo.angular2.grid.filter';
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    WjGridModule,
+    WjGridFilterModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
